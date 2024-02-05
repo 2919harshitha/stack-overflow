5d47492f03f48a8dfb6b8c44bffb53817a11e9dc
2919harshitha <harshithasm019@gmail.com>
2024-02-05 21:18:35 +0530

    add

--- server/index.js
+++ server/index.js
@@ -5,13 +5,14 @@ import questionRoutes from "./routes/Questions.js";
 import answerRoutes from "./routes/Answers.js";
 import connectDB from "./connectMongoDb.js";
 import dotenv from "dotenv";
+app.use(cors());
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
 
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
